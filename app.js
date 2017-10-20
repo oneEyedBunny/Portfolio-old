ab40eb507ec9792aabdd5a72f09ac2b783f71d15
oneEyedBunny <allyson7579@yahoo.com>
2017-10-19 21:28:27 -0700

    started working on object creation and constructor

---
+++ app.js
@@ -0,0 +1,17 @@
+// constructor for my projects object
+var Project = function (projects) {
+  name = projects.name;
+  img = projects.img;
+  desc = projects.desc;
+  gitRepo = projects.gitRepo;
+  tech = projects.tech;
+}
+// object of my work projects
+var projects = [
+  {name: "Market Analysis",
+  img: "images/XXX.jpg",
+  desc: "Blahblah",
+  gitRepo:"https://github.com/oneEyedBunny/market-analysis-app",
+  tech: "Blah",
+  }
+]
