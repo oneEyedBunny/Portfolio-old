80eae895b317360d352c90ddc7ab27a83ab3dcf0
oneEyedBunny <allyson7579@yahoo.com>
2017-10-22 09:24:13 -0700

    added gitignore, eslint, and readme. Also finished creating objects. Text needs to be modified still

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Portfolio
+These files display a website that is about me, the author and my work.
+
+If a bug is found, please email me at allyson7579@yahoo.com
--- app.js
+++ app.js
@@ -8,10 +8,22 @@ var Project = function (projects) {
 }
 // object of my work projects
 var projects = [
-  {name: "Market Analysis",
+  {name: "Market Analysis App",
   img: "images/XXX.jpg",
   desc: "Blahblah",
   gitRepo:"https://github.com/oneEyedBunny/market-analysis-app",
   tech: "Blah",
-  }
+},
+  {name: "Crop Swap",
+  img: "images/crop_swap.jpg",
+  desc: "Blahblah",
+  gitRepo:"https://github.com/oneEyedBunny/Crop-Swap",
+  tech: "Focus was on JavaScript",
+},
+  {name: "Cookie Stand App",
+  img: "images/XXX.jpg",
+  desc: "Blahblah",
+  gitRepo:"https://github.com/oneEyedBunny/cookie-stand",
+  tech: "Blah",
+},
 ]
